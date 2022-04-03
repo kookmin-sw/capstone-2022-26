c559d26f04e84f0517db47e368aada285657e81e
womyo <inhoking@kookmin.ac.kr>
2022-04-03 21:00:24 +0900

    [back-0001] feat. create docs folder

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,14 +13,14 @@ class ChartView(viewsets.ViewSet):
     queryset = Chart.objects.all()
 
     def list(self, request):
-        chartData = {'name':'n', 'artist': 'a', 'rank': 1, 'like':1, 'img_url':'https://aa.com'}
+        chartData = {'tracks': [{'track': 'GANADARA (Feat. 아이유)', 'artist': '박재범', 'rank': 1, 'img_url':'https://cdnimg.melon.co.kr/cm2/album/images/108/89/981/10889981_20220311110820_500.jpg/melon/resize/120/quality/80/optimize'}]}
         return Response(chartData)
     
 
     @action(detail=False, methods=['get'])
     def melon(self, request):
-        chartData = {'name':'melon', 'artist': 'a', 'rank': 1, 'like':1, 'img_url':'https://aa.com'}
-        return Response(chartData)
+        melonChartData = {'site':'Melon', 'tracks': [{'track': 'GANADARA (Feat. 아이유)', 'artist': '박재범', 'rank': 1, 'like':86592, 'img_url':'https://cdnimg.melon.co.kr/cm2/album/images/108/89/981/10889981_20220311110820_500.jpg/melon/resize/120/quality/80/optimize'}]}
+        return Response(melonChartData)
 
     @action(detail=False, methods=['get'])
     def bugs(self, request):
@@ -37,4 +37,5 @@ class TrackView(APIView):
     queryset = Track.objects.all()
     def get(self, request, pk):
         param = request.GET.get('artist')
-        return Response(pk + ' ' + param)
+        trackData = {'track':pk, 'artist': param, 'rank': [{'site': 'Melon', 'dayBefore': 2, 'yesterday': 2, 'today': 1}, {'site': 'Bugs', 'dayBefore': 3, 'yesterday': 4, 'today': 4}, {'site': 'Genie', 'dayBefore': 2, 'yesterday': 2, 'today': 3}]}
+        return Response(trackData)
