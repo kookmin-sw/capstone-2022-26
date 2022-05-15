601376135b1070bd4fa8f1b36ab42a6a329a27f3
ParkTB <ppttbb9461@naver.com>
2022-05-15 13:30:35 +0900

    [back-0001] chore. change column name

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -4,14 +4,14 @@ from .models import Melon, Bugs, Genie
 class MelonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Melon
-        fields = ('rank', 'song', 'artist', 'like', 'coverImg', 'date', 'time', 'weight')
+        fields = ('m_rank', 'm_song', 'm_artist', 'm_like', 'm_coverImg', 'm_date', 'm_time', 'm_weight')
 
 class GenieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genie
-        fields = ('rank', 'song', 'artist', 'like', 'coverImg', 'date', 'time', 'weight')
+        fields = ('g_rank', 'g_song', 'g_artist', 'g_like', 'g_coverImg', 'g_date', 'g_time', 'g_weight')
 
 class BugsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bugs
-        fields = ('rank', 'song', 'artist', 'like', 'coverImg', 'date', 'time', 'weight')
+        fields = ('b_rank', 'b_song', 'b_artist', 'b_like', 'b_coverImg', 'b_date', 'b_time', 'b_weight')
