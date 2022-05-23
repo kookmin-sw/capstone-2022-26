4b28bb5371e6f8691ebe273fb33d65bd73fcabfa
Seo BeomSeok <soulstair@gmail.com>
2022-05-24 01:48:15 +0900

    [front-0007] feat. add youtube with iframe

--- frontend/src/containers/DashboardContainer.jsx
+++ frontend/src/containers/DashboardContainer.jsx
@@ -16,6 +16,7 @@ const options = {
 
 function DashboardContainer() {
   const [chartData, setChartData] = useState({labels: [], datasets: []})
+  const [updateTime, setUpdateTime] = useState('');
   const [isLoading, setisLoading] = useState(true);
 
   const getData = async () => {
@@ -27,7 +28,18 @@ function DashboardContainer() {
     const data = getData();
     data.then(e => {
       setChartData(e);
-    })
+      const upTime = e.labels[e.labels.length-1];
+      const today = new Date();
+      if (upTime === today.getHours()) {
+        setUpdateTime(
+          `${today.getFullYear()}년
+           ${today.getMonth()+1}월
+           ${today.getDate()}일
+           ${upTime}시 업데이트`);
+      } else {
+        setUpdateTime(`${upTime}시 데이터 업데이트 중...`)
+      }
+    });
     setisLoading(false);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -37,10 +49,11 @@ function DashboardContainer() {
   return (
     <div style={{width: '70vw', height: '50vh', margin: '0 auto', marginTop: '10vh', backgroundColor: 'white', border: '1px solid #EFEFEF', borderRadius: '1em'}}>
       <div style={{textAlign: 'center', fontSize: '1.5em', margin: '3vh', fontWeight: '300', color: '#261C4C'}}> Top 10 Songs Chart </div>
+      <div style={{textAlign: 'right', fontSize: '0.8em', color: 'gray', marginRight: '10vw'}}>{updateTime}</div>
       <Line
         data={chartData}
         options={options}
-        style={{padding: '1em', backgroundColor: 'white', borderBottom: '1px solid #EFEFEF', borderRadius: '1em'}}
+        style={{padding: '1em', paddingTop: '0px', backgroundColor: 'white', borderBottom: '1px solid #EFEFEF', borderRadius: '1em'}}
       />
     </div>
   );
